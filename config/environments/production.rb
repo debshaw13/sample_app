9455ba2697d7714bd917f7f1e532e6a4d09cbab9
debshaw13 <debashishshaw@gmail.com>
2021-03-16 10:31:50 +0000

    Add account activation - bugs fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://git.heroku.com/protected-mesa-87808.herokuapp.com'
+  host = 'protected-mesa-87808.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
